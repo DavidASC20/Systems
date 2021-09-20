c5eed56bb7bf57f2d26135bc323211516591e497
DavidASC20 <djchong24@gmail.com>
2021-09-19 20:45:38 -0400

    finished playing around

---
+++ hw1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("whats poppin g \n");
+    printf("nuthin much mah boi \n");
+
+    char xGrade = 'A';
+    char yGrade = 'C';
+    char teacher[] = "Ms Heather";
+    float gpa = 3.72;
+
+    printf("bro I got a %c, Im so freaking happy man \n", xGrade);
+    printf("bro you mad lucky, I got a %c, %s hates me, my gpas gonna drop down to a %f", yGrade, teacher, gpa);
+    return 0;
+
+}
